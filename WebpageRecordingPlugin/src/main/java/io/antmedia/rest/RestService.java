c599dbd34f97b94108746e85b23ca24669556d1b
Mustafa BOLEKEN <mustafaboleken@yahoo.com>
2023-05-06 17:07:23 +0300

    Handle url without port

--- WebpageRecordingPlugin/src/main/java/io/antmedia/rest/RestService.java
+++ WebpageRecordingPlugin/src/main/java/io/antmedia/rest/RestService.java
@@ -2,7 +2,6 @@ package io.antmedia.rest;
 
 import javax.servlet.ServletContext;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -10,7 +9,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 import io.antmedia.plugin.WebpageRecordingPlugin;
@@ -42,7 +40,7 @@ public class RestService {
 		
 		String websocketScheme = ("https".equals(uriInfo.getBaseUri().getScheme())) ? "wss" : "ws";
 		String applicationName = uriInfo.getBaseUri().getPath().split("/")[1];
-		String websocketUrl = websocketScheme + "://" + uriInfo.getBaseUri().getHost() + ":" + uriInfo.getBaseUri().getPort() + "/" + applicationName + "/websocket";
+		String websocketUrl = websocketScheme + "://" + uriInfo.getBaseUri().getHost() + ((uriInfo.getBaseUri().getPort() != -1 ) ? ":" + uriInfo.getBaseUri().getPort() + "/" : "/") + applicationName + "/websocket";
 
 		WebpageRecordingPlugin app = getPluginApp();
 
