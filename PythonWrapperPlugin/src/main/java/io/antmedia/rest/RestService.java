93c8aed1ad821ec362d3eaf09a01462b36144d89
mekya <ahmetmermerkaya@gmail.com>
2023-11-25 19:37:46 +0300

    Update 2.8.0-SNAPSHOT

--- PythonWrapperPlugin/src/main/java/io/antmedia/rest/RestService.java
+++ PythonWrapperPlugin/src/main/java/io/antmedia/rest/RestService.java
@@ -1,23 +1,20 @@
 package io.antmedia.rest;
 
-import javax.servlet.ServletContext;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.context.WebApplicationContext;
 
 import io.antmedia.plugin.PythonWrapperPlugin;
+import jakarta.servlet.ServletContext;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 
 @Component
 @Path("/v1/python-wrapper-plugin")
