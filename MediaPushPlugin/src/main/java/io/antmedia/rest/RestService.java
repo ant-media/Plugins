ea02e1cc736aca52ab475d79243e8f211bc639d0
Mustafa BOLEKEN <mustafaboleken@yahoo.com>
2023-12-10 10:05:28 +0300

    Update Rest Service version number

--- MediaPushPlugin/src/main/java/io/antmedia/rest/RestService.java
+++ MediaPushPlugin/src/main/java/io/antmedia/rest/RestService.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.context.WebApplicationContext;
 
 @Component
-@Path("/v1/media-push")
+@Path("/v2/media-push")
 public class RestService {
 
 	@Context
