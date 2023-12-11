43a8af24f93c5af0ac990ab1751dde2c6a975a1f
Mustafa BOLEKEN <mustafaboleken@yahoo.com>
2023-12-11 16:56:56 +0300

    Revert increase rest version

--- MediaPushPlugin/src/main/java/io/antmedia/rest/RestService.java
+++ MediaPushPlugin/src/main/java/io/antmedia/rest/RestService.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.context.WebApplicationContext;
 
 @Component
-@Path("/v2/media-push")
+@Path("/v1/media-push")
 public class RestService {
 
 	@Context
