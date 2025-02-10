9b0527f072ff64cbb5e6a56047cf4370c9fa8e3a
USAMAWIZARD <usamawizard@gmail.com>
2025-02-10 20:36:25 +0530

    build opencv from src for Gstreamer support

---
+++ PythonPlugin/redeploy.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+AMS_DIR=/usr/local/antmedia
+mvn clean install -Dmaven.javadoc.skip=true -Dmaven.test.skip=true -Dgpg.skip=true
+OUT=$?
+
+if [ $OUT -ne 0 ]; then
+    exit $OUT
+fi
+
+cd ./src/main/python/
+python3 setup.py build_ext --inplace
+cd ../../../
+
+cp ./src/main/python/libpythonWrapper.so $AMS_DIR/lib/native/
+
+rm -r $AMS_DIR/plugins/PythonPlugin.jar
+cp ./target/PythonPlugin.jar $AMS_DIR/plugins/
+
+OUT=$?
+
+if [ $OUT -ne 0 ]; then
+    exit $OUT
+fi
+cd $AMS_DIR
+./start-debug.sh
