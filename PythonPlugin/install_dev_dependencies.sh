ee392c90055827c050e7afaf6537c7c04d732c96
USAMAWIZARD <usamawizard@gmail.com>
2025-03-27 02:07:43 +0530

    imporove  usablity

--- PythonPlugin/install_dev_dependencies.sh
+++ PythonPlugin/install_dev_dependencies.sh
@@ -2,9 +2,13 @@
 sudo apt install openjdk-17-jdk-headless
 sudo apt install maven
 sudo apt install python3-pip
+sudo apt install libavformat-dev libavcodec-dev libavutil-dev libswscale-dev
 sudo apt-get install libavutil-dev libavfilter-dev libavformat-dev libsdl2-dev libavcodec-dev libx264-dev libxvidcore-dev libvdpau-dev libva-dev libxcb-shm0-dev libwavpack-dev libvpx-dev libvorbis-dev libogg-dev libvidstab-dev libspeex-dev libopus-dev libopencore-amrnb-dev libopencore-amrwb-dev libmp3lame-dev libfreetype6-dev libfdk-aac-dev libass-dev libbz2-dev libsoxr-dev
-sudo at install ffmpeg
+sudo apt install ffmpeg
 
+sudo pip install cython opencv-python
+
+sudo pip install opencv
 sudo apt install cmake
 sudo apt install git
 sudo apt-get install python3-dev
